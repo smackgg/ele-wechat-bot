2f036c1e7d50c15a72ca08019d58d44e85302d28
smackxu <smackxu@raventech.com>
2018-02-01 17:30:29 +0800

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+const Wechat = require('wechat4u')
+const qrcode = require('qrcode')
+let bot = new Wechat()
+bot.start()
+
+bot.on('uuid', uuid => {
+  console.log('二维码链接：', 'https://login.weixin.qq.com/qrcode/' + uuid)
+})
+
+bot.on('message', msg => {
+  switch (msg.MsgType) {
+    case 37:
+      const { UserName, Ticket, Content } = msg.RecommendInfo;
+      // return
+      if (Content == '爸爸') {
+        bot.verifyUser(UserName, Ticket)
+      }
+      break
+    case bot.CONF.MSGTYPE_STATUSNOTIFY:
+      console.log(1);
+      break
+    case bot.CONF.MSGTYPE_TEXT:
+      bot.sendMsg(msg.Content, msg.FromUserName)
+      .catch(err => {
+        console.log(err)
+      })
+      break
+    case 49:
+      bot.sendMsg(msg.Url || msg.Content, msg.FromUserName)
+      .catch(err => {
+        console.log(err)
+      })
+      break
+  }
+})
