0c4f8c734010c1132d16164974a8c76ca9eb2227
smackgg <smackgg@gmail.com>
2018-02-02 15:56:55 +0800

    ele

--- index.js
+++ index.js
@@ -6,17 +6,26 @@ bot.on('uuid', uuid => {
   console.log('二维码链接：', 'https://login.weixin.qq.com/qrcode/' + uuid)
 })
 
+function handleEle(msg) {
+  const urls = msg.Url.split('#hardware_id')
+  const url = `${urls[0]}?from=singlemessage&amp;isappinstalled=0#hardware_id${urls[1]}`
+  const hongbaoUrl = `https://hrbust.smackgg.cn/test-ele?url=${encodeURIComponent(url.replace(/&amp;/g, '&'))}`
+  bot.sendMsg(hongbaoUrl, msg.FromUserName)
+  .catch(err => {
+    console.log(err)
+  })
+}
+
 bot.on('message', msg => {
   switch (msg.MsgType) {
     case 37:
       const { UserName, Ticket, Content } = msg.RecommendInfo;
       // return
-      if (Content == '爸爸') {
+      if (Content == '测试测试' || Content == '岩哥牛逼') {
         bot.verifyUser(UserName, Ticket)
       }
       break
     case bot.CONF.MSGTYPE_STATUSNOTIFY:
-      console.log(1);
       break
     case bot.CONF.MSGTYPE_TEXT:
       bot.sendMsg(msg.Content, msg.FromUserName)
@@ -25,10 +34,7 @@ bot.on('message', msg => {
       })
       break
     case 49:
-      bot.sendMsg(msg.Url || msg.Content, msg.FromUserName)
-      .catch(err => {
-        console.log(err)
-      })
+      handleEle(msg)
       break
   }
 })
